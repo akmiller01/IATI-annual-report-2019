26d9356483954f4b560f0ef00ca4c9de5afff45b
Alex Miller <alex.k.miller@gmail.com>
2019-01-22 12:45:16 -0500

    All currencies

--- currency.R
+++ currency.R
@@ -19,16 +19,27 @@ wdat <- wdat[complete.cases(wdat),]
 names(wdat) <- make.names(names(wdat))
 wdat$ex.rate <- wdat$U.S..dollars/wdat$National.currency
 
+ccs = fread("currency_codes.csv")
+ccs = subset(ccs,is.na(duplicate))
+keep = c("ISO","cc")
+ccs = ccs[,keep,with=F]
+
+wdat = merge(wdat,ccs,by="ISO")
+
 ex_list = list()
 
-for(iso in unique(wdat$ISO)){
+for(this.cc in unique(wdat$cc)){
   sub_list = list()
-  wdat_sub = subset(wdat,ISO==iso)
+  wdat_sub = subset(wdat,cc==this.cc)
   for(i in 1:nrow(wdat_sub)){
     sub_list[as.character(wdat_sub[i,"year"])] = wdat_sub[i,"ex.rate"]
   }
-  ex_list[[iso]] = sub_list
+  ex_list[[this.cc]] = sub_list
 }
+
+# Bitcoin transaction in 2015
+xbt_list = list("2015"=230.54)
+ex_list[["XBT"]] = xbt_list
 
 ex_json  = toJSON(ex_list, auto_unbox=T)
 write(ex_json,"ex_rates.json")
